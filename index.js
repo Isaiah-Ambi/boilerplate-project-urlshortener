b8ac29d1c3804c707a7eed454c31d0b0a23fb546
Isaiah-Ambi <ambiisaiah24@gmail.com>
2024-03-11 20:49:26 +0100

    init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { urlencoded } = require('body-parser');
 const app = express();
 
 // Basic Configuration
@@ -10,6 +11,8 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -18,6 +21,26 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+urls = [];
+
+app.get('/api/short/url/:id',(req,res) =>{
+  const id = req.params.id;
+  const url = urls.find(u => u.id === id);
+  res.redirect(url);
+});
+
+
+app.route('/api/shorturl')
+.get((req,res)=>{
+  res.send("Hi");
+})
+.post((req, res) =>{
+
+  const url = req.body.url;
+  urls.push(url)
+  res.json({original_url:url,short_url:urls.indexOf(url)})
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
